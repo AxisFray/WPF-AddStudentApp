76e012b2ba0a8ba1e4e23dc1a7ad1ec956bea740
AxisFray <113852846+AxisFray@users.noreply.github.com>
2025-05-05 12:34:31 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/AddStudent.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddStudent.xaml
+    /// </summary>
+    /// 
+
+
+
+    public class TextBoxMetadata
+    {
+        public string Placeholder { get; set; }
+        public bool IsRequired { get; set; }
+        public bool IsSet { get; set; } = false;
+    }
+
+    public partial class AddStudent : Window
+    {
+        public int m_bCloseMode = 0;
+        Color m_warnBgColor = Colors.Red;
+        Color m_warnFgColor = Colors.White;
+        Color m_placeholderFgColor = Colors.LightGray;
+        Color m_stdBgColor = Colors.White;
+        Color m_stdFgColor = Colors.Black;
+
+        private Dictionary<TextBox, TextBoxMetadata> textBoxDictionary = new Dictionary<TextBox, TextBoxMetadata>();
+
+        public AddStudent()
+        {
+            InitializeComponent();
+
+            textBoxDictionary[strName] = new TextBoxMetadata { Placeholder = "Imię ucznia", IsRequired = true };
+            textBoxDictionary[strSecName] = new TextBoxMetadata { Placeholder = "Drugie imię ucznia", IsRequired = false };
+            textBoxDictionary[strSurname] = new TextBoxMetadata { Placeholder = "Nazwisko ucznia", IsRequired = true };
+            textBoxDictionary[strPESEL] = new TextBoxMetadata { Placeholder = "Numer PESEL", IsRequired = true };
+
+
+            foreach (var textBox in textBoxDictionary.Keys)
+            {
+                setPlaceholder(textBox, textBoxDictionary[textBox].Placeholder);
+                textBox.GotFocus += (s, e) =>
+                {
+                    if (!textBoxDictionary[textBox].IsSet)
+                    {
+                        textBox.Text = "";
+                        textBox.Foreground = new SolidColorBrush(m_stdFgColor);
+                        textBox.Background = new SolidColorBrush(m_stdBgColor);
+                        textBoxDictionary[textBox].IsSet = true;
+                    }
+                };
+                textBox.LostFocus += (s, e) =>
+                {
+                    if (string.IsNullOrWhiteSpace(textBox.Text))
+                    {
+                        setPlaceholder(textBox, textBoxDictionary[textBox].Placeholder);
+                        textBoxDictionary[textBox].IsSet = false;
+                    }
+                };
+            }
+
+            void setPlaceholder(TextBox textBox, string placeholder)
+            {
+                textBox.Text = placeholder;
+                textBox.Foreground = new SolidColorBrush(m_placeholderFgColor);
+                textBox.Background = new SolidColorBrush(m_stdBgColor);
+            }
+
+        }
+
+        private bool checkPesel()
+        {
+            string pesel = strPESEL.Text;
+            strPESEL.Background = new SolidColorBrush(m_warnBgColor);
+            strPESEL.Foreground = new SolidColorBrush(m_warnFgColor);
+            if (pesel.Length != 11)
+            {
+
+                return false;
+            }
+            if (!long.TryParse(pesel, out _))
+            {
+
+                return false;
+            }
+
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += (pesel[i] - '0') * weights[i];
+            }
+            int controlDigit = (10 - (sum % 10)) % 10;
+            if (controlDigit != (pesel[10] - '0'))
+            {
+
+                return false;
+            }
+            strPESEL.Background = new SolidColorBrush(m_stdBgColor);
+            strPESEL.Foreground = new SolidColorBrush(m_stdFgColor);
+            return true;
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            int errorCount = 0;
+            foreach (var textBox in textBoxDictionary.Keys)
+            {
+                if (textBoxDictionary[textBox].IsRequired && !textBoxDictionary[textBox].IsSet)
+                {
+                    textBox.Background = new SolidColorBrush(m_warnBgColor);
+                    errorCount++;
+
+                }
+            }
+            if (errorCount > 0)
+            {
+                MessageBox.Show("Wypełnij wszystkie wymagane pola!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            foreach (var textBox in textBoxDictionary.Keys)
+            {
+                if (!textBoxDictionary[textBox].IsRequired)
+                {
+                    textBox.Text = "";
+                }
+            }
+
+            if (!checkPesel())
+            {
+                MessageBox.Show("Nieprawidłowy numer PESEL", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            m_bCloseMode = 1;
+            Close();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    class Osoba
+    {
+        public string? m_strPESEL { get; set; }
+        public string? m_strName { get; set; }
+        public string? m_strSecName { get; set; }
+        public string? m_strSurname { get; set; }
+
+        public string? m_strBirthDate { get; set; }
+        public Osoba()
+        {
+            m_strPESEL = "00000000000";
+            m_strName = "";
+            m_strSecName = "";
+            m_strSurname = "";
+            m_strBirthDate = "0000.00.00";
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mainList.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath, encoding);
+                    foreach (var line in lines)
+                    {
+                        string[] columns = line.Split(delimiter);
+                        if (columns != null)
+                        {
+                            Osoba uczen = new();
+                            uczen.m_strPESEL = columns.ElementAtOrDefault(0);
+                            uczen.m_strName = columns.ElementAtOrDefault(1);
+                            uczen.m_strSecName = columns.ElementAtOrDefault(2);
+                            uczen.m_strSurname = columns.ElementAtOrDefault(3);
+                            mainList.Items.Add(uczen);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz plik CSV";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Osoba item in mainList.Items)
+                    {
+                        var row = $"{item.m_strPESEL}{delimiter}{item.m_strName}" +
+                            $"{delimiter}{item.m_strSecName}{delimiter}{item.m_strSurname}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void RemoveSel_Click(object sender, RoutedEventArgs e)
+        {
+            while (mainList.SelectedItems.Count > 0)
+            {
+                mainList.Items.Remove(mainList.SelectedItems[0]);
+            }
+        }
+
+        private void NewRecord_Click(object sender, RoutedEventArgs e)
+        {
+
+            AddStudent wnd = new();
+            wnd.ShowDialog();
+
+
+
+            if (wnd.m_bCloseMode == 1)
+            {
+                Osoba uczen = new();
+                uczen.m_strPESEL = wnd.strPESEL.Text;
+                uczen.m_strName = wnd.strName.Text;
+                uczen.m_strSecName = wnd.strSecName.Text;
+                uczen.m_strSurname = wnd.strSurname.Text;
+                uczen.m_strBirthDate = wnd.dataUrodzenia.ToString().Split()[0];
+                mainList.Items.Add(uczen);
+            }
+
+
+        }
+    }
+}
